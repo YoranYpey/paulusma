cb9a6bec56222886e4e4086b9fde354ad00ed3eb
davet <davethijs@hotmail.com>
2018-01-18 16:51:59 +0100

    -

--- src/Consumer.java
+++ src/Consumer.java
@@ -22,30 +22,33 @@ public class Consumer implements Runnable{
         try {
             String msg;
             String station = "";
-            while ((msg = queue.take())!= null) {
-                if(msg.contains("STN")){
-                    station = msg.substring(7, msg.length() - 6);
-                    synchronized (queue) {
-                        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                        Date date = new Date();
-                        String path = System.getProperty("user.dir") + "\\station_data\\" + station + "\\" + dateFormat.format(date);
+            if(queue.size() == 14){
+                while ((msg = queue.take())!= null) {
+                    if (msg.contains("STN")) {
+                        station = msg.substring(7, msg.length() - 6);
+                        synchronized (queue) {
+                            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                            Date date = new Date();
+                            String path = System.getProperty("user.dir") + "\\station_data\\" + station + "\\" + dateFormat.format(date);
 
-                        //Set directory and file
-                        Path dir = Paths.get(path);
-                        Path file = dir.resolve("data.txt");
+                            //Set directory and file
+                            Path dir = Paths.get(path);
+                            Path file = dir.resolve("data.txt");
 
-                        try {
-                            //Check if the directory exists, if not create it
-                            if (!Files.exists(dir)) {
-                                Files.createDirectories(dir);
+                            try {
+                                //Check if the directory exists, if not create it
+                                if (!Files.exists(dir)) {
+                                    Files.createDirectories(dir);
+                                }
+                                //Check if the file exists, if not create it
+                                if (!Files.exists(file)) {
+                                    Files.createFile(file);
+                                }
+                                System.out.println(queue);
+                                Files.write(file, queue, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
                             }
-                            //Check if the file exists, if not create it
-                            if (!Files.exists(file)) {
-                                Files.createFile(file);
-                            }
-                            Files.write(file, queue, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
                         }
                     }
                 }
