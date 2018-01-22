76fd6462fe659ba0f2144ccfe5793e374d0c5ebb
Dave <davethijs@hotmail.com>
2018-01-22 15:37:33 +0100

    Missing values....

--- src/Handler.java
+++ src/Handler.java
@@ -1,11 +1,25 @@
-import java.net.Socket;
-import java.util.Arrays;
-import java.util.concurrent.BlockingQueue;
+import org.apache.commons.io.input.ReversedLinesFileReader;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 
 public class Handler implements Runnable{
+
+    static int counter;
 
     private BlockingQueue<String> queue;
     private BufferedReader buf;
@@ -21,11 +35,10 @@ public class Handler implements Runnable{
 
     public void run(){
             try {
+                buf.readLine();
                 while (true) {
                     if (buf.readLine().contains("<measure".toUpperCase())) {
                         writeQueue();
-                        //Thread thread = new Thread(new Parser(queue));
-                        //thread.start();
                     } else {
                         buf.readLine();
                     }
@@ -42,8 +55,12 @@ public class Handler implements Runnable{
                     try {
                         String c = buf.readLine();
                         if(stringContainsItemFromList(c, itemList)) {
+                            if(!(c.matches(".*\\d.*"))){
+                                System.out.println("Missing Value");
+                                calcValue(c);
+                            }
                             queue.put(c);
-                            System.out.println(c);
+                            //System.out.println(c);
                         }
                     } catch (InterruptedException ex) {
                         ex.printStackTrace();
@@ -57,6 +74,56 @@ public class Handler implements Runnable{
 
     private static boolean stringContainsItemFromList(String inputStr, String[] items) {
         return Arrays.stream(items).parallel().anyMatch(inputStr::contains);
+    }
+
+    private String getStation() {
+        String msg = queue.peek();
+        String station = "";
+        if (msg.contains("STN")) {
+            station = msg.substring(7, msg.length() - 6);
+        }
+        return station;
+    }
+
+    private String calcValue(String type) throws IOException{
+        String type1 = type.split(">")[0];
+        List<Double> values = new ArrayList<>();
+        String station = getStation();
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = new Date();
+        String path = System.getProperty("user.dir") + "\\station_data\\" + station + "\\" + dateFormat.format(date);
+
+        //Set directory and file
+        Path dir = Paths.get(path);
+        Path file = dir.resolve("data.txt");
+        File file2 = file.toFile();
+
+        ReversedLinesFileReader revLineRead = new ReversedLinesFileReader(file2, Charset.forName("UTF-8"));
+
+        System.out.println("Reading file.......");
+
+        for(int i = 0; i < 180; i++) {
+            String line = revLineRead.readLine();
+            if (line.contains(type1)) {
+                line = line.replaceAll("[^\\d.:-]", "");
+                values.add(Double.parseDouble(line));
+            }
+        }
+
+
+
+        double sum = 0;
+        for(Double d : values){
+            sum+= d;
+        }
+
+        DecimalFormat df = new DecimalFormat("#0.00");
+        String missingVals = df.format(sum/30);
+        String[] split = type.split("(?<=>)");
+        String missingVal = (split[0] + missingVals + split[1]);
+        System.out.println(missingVal);
+
+        return missingVal;
     }
 
 }
