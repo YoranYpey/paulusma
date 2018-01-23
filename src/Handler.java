979f5a6aa6b47916db0990e04e622eb984214b7f
davet <davethijs@hotmail.com>
2018-01-23 12:21:43 +0100

    Merge conflict fixed

--- src/Handler.java
+++ src/Handler.java
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.util.ArrayList;
-
-public class Handler implements Runnable {
-	
-	public Socket client;
-	public int client_no;
-	public Listener listener;
-	public Parser parser;
-	
-	public Handler(Socket client,int client_no, Listener listener) {
-		this.client = client;
-		this.client_no = client_no;
-		this.listener = listener;
-		parser = new Parser();
-		System.out.println("Ready to receive data from client number: "+client_no);
-	}
-
-
-	public void run() {
-		try {
-			String clientMessage = null;
-			BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream(),"UTF-8"));
-			int counter = 0;
-			while ((clientMessage = fromClient.readLine())!= null) {
-				if (clientMessage.equals("	<MEASUREMENT>")) {
-					ArrayList<String> MessageList = new ArrayList<String>();
-					System.out.println("Received a new XML-Entry from: "+client_no);
-					while(counter<14) {
-					clientMessage = fromClient.readLine();
-					MessageList.add(clientMessage);
-					counter++;
-					}
-				if(counter == 14) {
-					//parser.handleArrayList(MessageList);
-					parser.parseQueue(MessageList);
-					counter =0;
-					}
-				}
-			}
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-		
-}	
-}
-=======
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
@@ -111,4 +60,3 @@ public class Handler implements Runnable{
     }
 
 }
->>>>>>> 01c9b7e80a545147d0004d52a8f794327e7b3e14
